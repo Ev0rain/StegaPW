e2d1df01cdcdcdd1b17aed68001b7b1a488d3811
Ev0rain <evrainow@gmail.com>
2024-12-12 09:20:43 +0100

    encode_data V1

--- Main.py
+++ Main.py
@@ -16,6 +16,26 @@ def encode_data_in_image(image_path, data, output_path):
     new_pixels = []
     binary_index = 0
     for pixel in pixels:
-        if binary_index > len(binary_data):
+        if binary_index < len(binary_data):
             new_red = (pixel[0] & ~1) | int(binary_data[binary_index])
             binary_index += 1
+        else:
+            new_red = pixel[0]
+
+        if binary_index < len(binary_data):
+            new_green = (pixel[1] & ~1) | int(binary_data[binary_index])
+            binary_index += 1
+        else:
+            new_green = pixel[1]
+
+        if binary_index < len(binary_data):
+            new_blue = (pixel[2] & ~1) | int(binary_data[binary_index])
+            binary_index += 1
+        else:
+            new_green = pixel[2]
+
+        new_pixels.append((new_red, new_green, new_blue))
+
+    new_img = Image.new("RGB", img.size)
+    new_img.putdata(new_pixels)
+    new_img.save(output_path)
