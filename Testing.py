6ad7df0ccf8606f9f6aeae5527ad44ac8897edef
Ev0rain <evrainow@gmail.com>
2025-01-09 07:55:31 +0100

    Streamlit_Integration_V1

--- Testing.py
+++ Testing.py
@@ -2,18 +2,18 @@ import Main
 import os
 from PIL import Image
 
-""" if os.path.exists("encoded_image.png"):
-    os.remove("encoded_image.png")
-if os.path.exists("test_image.png"):
-    os.remove("test_image.png")
+# if os.path.exists("encoded_image.png"):
+#     os.remove("encoded_image.png")
+# if os.path.exists("test_image.png"):
+#     os.remove("test_image.png")
 
-img = Image.new("RGB", (100, 100), color="white")
-img.save("test_image.png")
+# img = Image.new("RGB", (100, 100), color="white")
+# img.save("test_image.png")
 
-# Testing encode feature
-Main.encode_data_in_image("test_image.png", "Hello, BCYB24", "encoded_image.png")
+# # Testing encode feature
+# Main.encode_data_in_image("test_image.png", "Hello, BCYB24", "encoded_image.png")
 
-Main.decode_data_from_image("encoded_image.png") """
+# Main.decode_data_from_image("encoded_image.png")
 
 Message = "Hello, World!"
 
